f6bed6eb3c525bd51be404d88cac58a18b1f799e
Luiz Henrique <luizhjramos@outlook.com>
2021-10-15 16:15:09 -0300

    feat: routes application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+// Packages
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Componets
 import Home from "./pages/Home";
 import NewRoom from "./pages/NewRoom";
 
 function App() {
-  return <NewRoom />;
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Home} exact />
+      <Route path="/rooms/new" component={NewRoom} exact />
+    </BrowserRouter>
+  );
 }
 
 export default App;
