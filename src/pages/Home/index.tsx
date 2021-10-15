83f070bc2110828764b604a5e7ab10d7bedfc837
Luiz Henrique <luizhjramos@outlook.com>
2021-10-15 10:15:48 -0300

    feat: Home page structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,39 @@
+// Packages
 import React from "react";
 
+// Assets
+import illustrationImg from "../../assets/images/illustration.svg";
+import logoImg from "../../assets/images/logo.svg";
+import googleIconImg from "../../assets/images/google-icon.svg";
+
 const Home: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
   return (
     <div>
-      <aside></aside>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Imagem simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiêcia em tempo real</p>
+      </aside>
+      <main>
+        <div>
+          <img src={logoImg} alt="Logo" />
+          <button type="button">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div>Ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">Entrar na sala</button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
