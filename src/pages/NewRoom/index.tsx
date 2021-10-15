c740522e74d14629520459b06125e4ce53d7dd82
Luiz Henrique <luizhjramos@outlook.com>
2021-10-15 15:55:41 -0300

    style: NewRoom component

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,6 +1,14 @@
 // Packages
 import React from "react";
 
+// Assets
+import illustrationImg from "../../assets/images/illustration.svg";
+import logoImg from "../../assets/images/logo.svg";
+import googleIconImg from "../../assets/images/google-icon.svg";
+
+// Components
+import Button from "../../components/Button";
+
 // Styles
 import styles from "./styles.module.scss";
 
@@ -8,7 +16,31 @@ const NewRoom: React.FC = () => {
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <div />;
+  return (
+    <div id={styles.page_auth}>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Imagem simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiêcia em tempo real</p>
+      </aside>
+      <main>
+        <div className={styles.main_content}>
+          <img src={logoImg} alt="Logo" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="button">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default NewRoom;
