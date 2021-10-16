b9837dcbeaefd057ff9cc532b908991e3c983864
Luiz Henrique <luizhjramos@outlook.com>
2021-10-16 19:39:29 -0300

    feat: Route Room page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 // Componets
 import Home from "./pages/Home";
 import NewRoom from "./pages/NewRoom";
+import Room from "./pages/Room";
 
 // context
 import { AuthContextProvider } from "./contexts/AuthContext";
@@ -17,6 +18,7 @@ function App() {
       <BrowserRouter>
         <Route path="/" component={Home} exact />
         <Route path="/rooms/new" component={NewRoom} exact />
+        <Route path="/rooms/:id" component={Room} exact />
       </BrowserRouter>
     </AuthContextProvider>
   );
