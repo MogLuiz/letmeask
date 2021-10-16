0f5bdf860477131fc8578204d20a307cdf4611da
Luiz Henrique <luizhjramos@outlook.com>
2021-10-16 15:00:19 -0300

    feat: AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,20 @@ import Home from "./pages/Home";
 import NewRoom from "./pages/NewRoom";
 
 // context
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({} as any);
 
 function App() {
+  const [user, setUser] = useState();
+
   return (
-    <BrowserRouter>
-      <Route path="/" component={Home} exact />
-      <Route path="/rooms/new" component={NewRoom} exact />
-    </BrowserRouter>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Route path="/" component={Home} exact />
+        <Route path="/rooms/new" component={NewRoom} exact />
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
