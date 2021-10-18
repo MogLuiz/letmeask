5c82d2e5bced0b3ca549f28232f72887fe17f19f
Luiz Henrique <luizhjramos@outlook.com>
2021-10-18 19:40:46 -0300

    feat: Structure Room page

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,7 +1,44 @@
+// Packages
 import React from "react";
 
+// Assets
+import logoImg from "../../assets/images/logo.svg";
+
+// Components
+import Button from "../../components/Button";
+
 const Room: React.FC = () => {
-  return <h1>Sala</h1>;
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return (
+    <div className="page_room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>código</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room_title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+
+          <div className="form_footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>.
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Room;
