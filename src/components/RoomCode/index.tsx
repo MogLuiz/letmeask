748862a9559a3cb12c043948a6c32f369f22791e
Luiz Henrique <luizhjramos@outlook.com>
2021-10-18 21:02:26 -0300

    feat: initialize RoomCode component

---
+++ src/components/RoomCode/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// Styles
+import styles from "./styles.module.scss";
+
+const RoomCode: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return <div />;
+};
+
+export default RoomCode;
